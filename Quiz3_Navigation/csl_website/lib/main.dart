88597f9b53e97f1ccb9650fc59de87e93b8131e2
Grace L.R. Pangaribuan <ges240706@gmail.com>
2026-02-04 13:20:05 +0700

    push quiz3

--- Quiz3_Navigation/csl_website/lib/main.dart
+++ Quiz3_Navigation/csl_website/lib/main.dart
@@ -1,121 +1,209 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PrasetiyaMulyaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PrasetiyaMulyaApp extends StatelessWidget {
+  const PrasetiyaMulyaApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Sans-Serif'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _showDialog(BuildContext context, String menuName) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        title: const Text(
+          "Informasi",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        content: Text("$menuName clicked"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("OK", style: TextStyle(color: Colors.blue)),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
+     
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.red,
+        elevation: 0,
+        centerTitle: false,
+        title: Image.network('https://www.prasetiyamulya.ac.id/wp-content/uploads/2020/01/Logo-Universitas-Prasetiya-Mulya.png',
+        height: 60,
+        fit: BoxFit.contain,
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+            onSelected: (value) => _showDialog(context, value),
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem(value: "Programs", child: Text("Programs")),
+              const PopupMenuItem(value: "Addmission", child: Text("Addmission")),
+              const PopupMenuItem(value: "People", child: Text("People")),
+              const PopupMenuItem(value: "Laboratory", child: Text("Laboratory")),
+              const PopupMenuItem(value: "Campus Life", child: Text("Campus Life")),
+              const PopupMenuItem(value: "Offices & Services", child: Text("Offices & Services")),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Divider(thickness: 1, height: 1),
+
+             Center(
+                child: Image.network(
+                  'https://www.prasetiyamulya.ac.id/wp-content/uploads/2020/01/Logo-Universitas-Prasetiya-Mulya.png',
+                  width: 350,
+                  height: 150,
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              // --- VISION SECTION ---
+              const SectionHeader(title: "VISION"),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  "A globally recognized School for STEMpreneur Education and Research",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              // --- MISSION SECTION ---
+              const SectionHeader(title: "MISSION"),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  "Provide quality STEM education and research for nurturing the holistic citizen graduates through:",
+                  style: TextStyle(fontSize: 16, color: Colors.black87),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(25, 20, 25, 40),
+                child: Text(
+                  "1. Collaborative learning by enterprising involving interdisciplinary catalytic projects\n"
+                  "2. Innovative and impactful research to the society",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+
+              // --- FOOTER ADDRESS ---
+              Container(
+                width: double.infinity,
+                height: 250,
+                padding: const EdgeInsets.all(25),
+                color: Colors.black, // Background hitam
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    Image.network(
+                      'https://www.prasetiyamulya.ac.id/office/publishing/wp-content/uploads/2021/03/Prasmul_logo_01-Putih.png',
+                      height: 60,
+                      fit: BoxFit.contain,
+                    ),
+
+                    Text(
+                      "BSD City Kavling Edutown I.1",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // Teks putih
+                      ),
+                    ),
+                    Text(
+                      "Jl. BSD Raya Utama, BSD City 15339",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "Kabupaten Tangerang, Indonesia",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Tel. (021) 304-50-500",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildMenuItem(BuildContext context, String title) {
+    return Column(
+      children: [
+        ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 25,
+            vertical: 5,
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              letterSpacing: 1.1,
+              fontSize: 14,
+            ),
+          ),
+          onTap: () => _showDialog(context, title),
+        ),
+        Divider(color: Colors.white.withOpacity(0.1), height: 1),
+      ],
+    );
+  }
+}
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  const SectionHeader({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 20, 25, 10),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w900,
+          color: Colors.black,
+        ),
       ),
     );
   }
